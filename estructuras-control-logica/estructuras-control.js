f7df76cc0e0f193b4700f9c18437a0b490aa8bb9
nicogithub05 <nicolasgithub@gmail.com>
2024-09-23 10:45:21 -0500

    operadores logicos y de comparacion

---
+++ estructuras-control-logica/estructuras-control.js
@@ -0,0 +1,52 @@
+// operadores de comparación
+/* 
+ == -> valida que el valor sea el mismo
+ === -> valida que el valor y el tipo de dato sea el mismo
+!= -> Negación (si es diferente true, si es igual false);
+!== diferente de -> Este valor es diferente al otro
+>
+<
+>=
+<=
+*/
+
+
+const a = 10;
+const b = 20;
+const c = "10";
+
+
+console.log(a == b); //false
+console.log(a == c); //true
+console.log(a === b); // false
+
+console.log(a != b); // true
+console.log(a !== c); // true
+
+console.log(a > b); //false
+
+console.log(b >= a) // true
+
+
+// ----- Operadores lógicos ---- //
+
+// && y
+// || or
+// ! no
+
+const d = 10;
+const e = 20;
+const f = "10";
+
+console.log(d == e && d === f);
+
+console.log(d != e || d === f)
+
+console.log(!(d === f))
+
+
+
+
+
+
+
