29e557f3f86bd763f588bcde911a13acb812e983
nicogithub05 <nicolasgithub@gmail.com>
2024-09-27 13:42:02 -0500

    funciones puras e impuras

--- funciones-this.js/funciones-this.js
+++ funciones-this.js/funciones-this.js
@@ -85,6 +85,46 @@ const rocket = {
 rocket.launchMessage();
 
 
+// funciones puras vs impuras
+
+// causas side effects = efectos secundarios -> Hace que las funciones dejen de ser puras
+// 1. Modificar variables globales
+// 2. Modificar parámetros
+// 3. Solicitudes HTTP
+// 4. Imprimir mensajes en pantalla o consola
+// 5. Manipulación del DOM.
+// 6. Obtener hora o día actual.
+
+function sum(a, b) { // funcion pura: se reciben los mismos parámetros y obtenemos la misma salida. 
+    return a + b;
+}
+
+function sum(a, b) {
+    console.log(`a: ${a}`);
+    return a + b; 
+}
 
 
+let total = 0;
+
+function sumWithSideEffect(a) {
+    total += a;
+    return total;
+}
+
+// Funciones puras
+
+function square(x) { 
+    return x * x; 
+}
+
+function addTen(y) {
+    return y + 10;
+}
+
+// funcion con funciones combinadas pero que sigue siendo pura
+
+const number = 5; 
+const finalResult = addTen(square(number))
+console.log(finalResult);
 
