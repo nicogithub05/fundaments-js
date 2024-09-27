fae7dadcd54ac1ca0a088c5d8f1a7f8d872f7341
nicogithub05 <nicolasgithub@gmail.com>
2024-09-27 13:17:12 -0500

    funciones vs métodos

--- funciones-this.js/funciones-this.js
+++ funciones-this.js/funciones-this.js
@@ -20,6 +20,69 @@ console.log(`discount: $${discountPercentage}%`);
 console.log(`price with discount: $${finalPrice}`);
 
 
+// funciones vs métodos. 
+
+// todo en javascript es un objeto. 
+
+// capacidades de las funciones: 
+
+// funciones como argumentos: callbacks
+
+function a() {
+
+}
+
+function b(a) {
+    b(a);
+}
+
+function a() {
+    function b() {
+        return b
+    }
+}
+
+// asignar funciones a variables 
+
+let a = function() {
+    console.log('soy una función dentro de una variable')
+} // el nombre de la funcion queda como nombre de la variable. 
+
+a();
+
+// estas funciones pueden tener propiedades y métodos: 
+
+
+let obj1 = {
+    nombre: 'nicolas',
+    apellido: 'valencia'
+}
+
+a.call(obj1); // método de una función que funciona como objeto (todo en javascript es un objeto);
+
+function a() {
+    function b() {
+        function c() {
+
+        }
+        c();
+    }
+    b()
+}
+
+a(); 
+
+
+// almacenar funciones en objetos: 
+
+const rocket = {
+    name: 'Falcon 9',
+    launchMessage: function() { // funcion almacenada dentro de un objeto:
+        console.log('El cohete se lanzó exitosamente')
+    }
+}
+
+rocket.launchMessage();
 
 
 
