a49308f24f51587ca4ef5ec4fc304948210ab5bd
nicogithub05 <nicolasgithub@gmail.com>
2024-10-11 09:58:33 -0500

    prototipos y herencias - clases y funciones constructoras

--- program-orientada-objetos/POO.js
+++ program-orientada-objetos/POO.js
@@ -54,6 +54,81 @@ Persona.prototype.saludar = function() {
 persona1.saludar();
 
 
+// 3 ---- Clases 
+
+// sugar sintax para que sea mas intuitivas las clases en JS
+
+class Persona { // arquetipo de la clase
+    constructor(name, lastName, age) {
+        this.name = name; 
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    saludar() {
+        console.log(`Hola, mi nombre es: ${this.name}`)
+    }
+}
+
+const persona3 = new Persona("Nicolas", 'Valencia', 28) // instancia de esa clase
+
+persona3.saludar() // metodo de la clase 
+
+// 4 ----- prototipos y herencias
+
+// cuando creamos el prototipo, las instancias que creemos van a heredar esa estructura para crear el objeto. 
+// prototype solo funciona en clases y funciones constructoras.
+
+class Animal { // molde de la clase para construir mas instancias
+    constructor(nombre, tipo) {
+        this.name = nombre;
+        this.type = tipo;
+    }
+    emitirSonido() {
+        console.log("El animal emite un sonido")
+    }
+}
+
+
+class Perro  extends Animal {
+    constructor(nombre, tipo, raza) {
+        super(nombre, tipo);
+        this.raza = raza; 
+    }
+
+    emitirSonido() {
+        console.log("El perro ladra");
+    }
+
+    correr() {
+        console.log(`${this.nombre} corre alegremente`)
+    }
+}
+
+const perro1 = new Perro("Noah", "Perro", "Pug");
+
+console.log(perro1);
+
+perro1.correr();
+
+perro1.emitirSonido();
+
+// prototipos 
+
+perro1.nuevoMetodo = function () { // aquí solo se agrega el método a la instancia no a la clase
+    console.log("este es un nuevo método")
+};
+
+perro1.nuevoMetodo();
+
+Perro.prototype.segundoMetodo = function() { // el método solo se agrega a las clases constructoras, no a instancias
+    console.log("este es un segundo método")
+}
+
+perro1.segundoMetodo();
+
+// todas las clases pertenecen a el objeto global Object. Es una cadena de prototipos.
+// perro1 tiene un protypo deribado de perro y la clase perro tiene un prototipo deribado de la clase Animal. 
 
 
 
